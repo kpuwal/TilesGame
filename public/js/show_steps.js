925009b8007fe37cbb63466e72d5b0d0b8b38e57
kpuwal <kpuwal@gmail.com>
2017-01-25 12:09:06 +0000

    refactors

--- public/js/show_steps.js
+++ public/js/show_steps.js
@@ -7,23 +7,23 @@ $("#show_steps").click(function() {
 function showSteps() {
   document.getElementById("show_steps").value="Next Step";
   var solution = document.getElementById('solved').value;
-  var move = ''
+  var move = '';
   switch(solution[step]){
     case "R":
-      move =  (emptytilePosRow).toString() + ',' + (emptytilePosCol + 1).toString()
-      break
+      move =  (emptytilePosRow).toString() + ',' + (emptytilePosCol + 1).toString();
+      break;
     case "L":
-      move =  (emptytilePosRow).toString() + ',' + (emptytilePosCol - 1).toString()
-      break
+      move =  (emptytilePosRow).toString() + ',' + (emptytilePosCol - 1).toString();
+      break;
     case "U":
-      move =  (emptytilePosRow - 1).toString() + ',' + (emptytilePosCol).toString()
-      break
+      move =  (emptytilePosRow - 1).toString() + ',' + (emptytilePosCol).toString();
+      break;
     case "D":
-      move =  (emptytilePosRow + 1).toString() + ',' + (emptytilePosCol).toString()
-      break
-  };
+      move =  (emptytilePosRow + 1).toString() + ',' + (emptytilePosCol).toString();
+      break;
+  }
 
-  $("div[data-pos='" + move + "']").click()
-  step_panel.innerHTML += ' Step: ' + step + ' -> ' + solution[step] + ','
-  step++
+  $("div[data-pos='" + move + "']").click();
+  step_panel.innerHTML += ' Step: ' + step + ' -> ' + solution[step] + ',';
+  step++;
 }
